46daf39faf44d9efeed85f9d7e876e6150121dd5
unknown <phamluongtai@gmail.com>
2017-11-10 19:25:53 -0800

    recheck all

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,147 +1,54 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 var expect = require('expect');
 
-import firebase, {firebaseRef} from 'app/firebase/';
-var actions = require('actions');
+var TodoAPI = require('TodoAPI');
 
-var createMockStore = configureMockStore([thunk]);
-
-describe('Actions', () => {
-  it('should generate search text action', () => {
-    var action = {
-      type: 'SET_SEARCH_TEXT',
-      searchText: 'Some search text'
-    };
-    var res = actions.setSearchText(action.searchText);
-
-    expect(res).toEqual(action);
-  });
-
-  it('should generate toggle show completed action', () => {
-    var action = {
-      type: 'TOGGLE_SHOW_COMPLETED'
-    };
-    var res = actions.toggleShowCompleted();
-
-    expect(res).toEqual(action);
-  });
-
-  it('should generate add todo action', () => {
-    var action = {
-      type: 'ADD_TODO',
-      todo: {
-        id: '123abc',
-        text: 'Anything we like',
-        completed: false,
-        createdAt: 0
-      }
-    };
-    var res = actions.addTodo(action.todo);
-
-    expect(res).toEqual(action);
-  });
-
-  it('should create todo and dispatch ADD_TODO', (done) => {
-    const store = createMockStore({});
-    const todoText = 'My todo item';
-
-    store.dispatch(actions.startAddTodo(todoText)).then(() => {
-      const actions = store.getActions();
-      expect(actions[0]).toInclude({
-        type: 'ADD_TODO'
-      });
-      expect(actions[0].todo).toInclude({
-        text: todoText
-      });
-      done();
-    }).catch(done);
-  });
-
-  it('should generate add todos action object', () => {
-    var todos = [{
-      id: '111',
-      text: 'anything',
-      completed: false,
-      completedAt: undefined,
-      createdAt: 33000
-    }];
-    var action = {
-      type: 'ADD_TODOS',
-      todos
-    };
-    var res = actions.addTodos(todos);
-
-    expect(res).toEqual(action);
-  });
-
-  it('should generate update todo action', () => {
-    var action = {
-      type: 'UPDATE_TODO',
-      id: '123',
-      updates: {completed: false}
-    };
-    var res = actions.updateTodo(action.id, action.updates);
-
-    expect(res).toEqual(action);
-  });
-
-  describe('Tests with firebase todos', () => {
-    var testTodoRef;
-
-    beforeEach((done) => {
-      var todosRef = firebaseRef.child('todos');
-
-      todosRef.remove().then(() => {
-        testTodoRef = firebaseRef.child('todos').push();
-
-        return testTodoRef.set({
-          text: 'Something to do',
-          completed: false,
-          createdAt: 23453453
-        })
-      })
-      .then(() => done())
-      .catch(done);
+describe('TodoAPI', () => {
+    beforeEach(() => {
+        localStorage.removeItem('todos');
     });
 
-    afterEach((done) => {
-      testTodoRef.remove().then(() => done());
+    it('should exist', () => {
+        expect(TodoAPI).toExist();
     });
 
-    it('should toggle todo and dispatch UPDATE_TODO action', (done) => {
-      const store = createMockStore({});
-      const action = actions.startToggleTodo(testTodoRef.key, true);
+    describe('filterTodos', () => {
+        var todos = [{
+            id: 1,
+            text: 'Some text here',
+            completed: true
+        },{
+            id: 2,
+            text: 'Other text here',
+            completed: false
+        },{
+            id: 3,
+            text: 'Some text here',
+            completed: true
+        }];
 
-      store.dispatch(action).then(() => {
-        const mockActions = store.getActions();
-
-        expect(mockActions[0]).toInclude({
-          type: 'UPDATE_TODO',
-          id: testTodoRef.key
+        it('should return all items if showCompleted is true', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
         });
-        expect(mockActions[0].updates).toInclude({
-          completed: true
+
+        it('should return non-completed todos when showCompleted is false', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
         });
-        expect(mockActions[0].updates.completedAt).toExist();
 
-        done();
-      }, done);
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by searchText', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+            expect(filteredTodos.length).toBe(2);
+        });
+
+        it('should return all todos if searchText is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
     });
-
-    it('should populate todos and dispatch ADD_TODOS', (done) => {
-      const store = createMockStore({});
-      const action = actions.startAddTodos();
-
-      store.dispatch(action).then(() => {
-        const mockActions = store.getActions();
-
-        expect(mockActions[0].type).toEqual('ADD_TODOS');
-        expect(mockActions[0].todos.length).toEqual(1);
-        expect(mockActions[0].todos[0].text).toEqual('Something to do');
-
-        done();
-      }, done)
-    });
-  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,7 @@ module.exports = {
               API_KEY: JSON.stringify(process.env.API_KEY),
               AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
               DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
-              STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
-              MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
+              STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET)
           }
       })
   ],
